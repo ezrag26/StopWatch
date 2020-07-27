b80a8b0441adc567210d3d555bd5e2936c084db7
Ezra Gordon <ezrag26@gmail.com>
2020-07-27 23:18:47 +0300

    add configuration to build js files

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "main_bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}
